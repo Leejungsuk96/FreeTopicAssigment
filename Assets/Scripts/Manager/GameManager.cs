9654070f58532828e455ecb572d97157ca73065f
Leejungsuk96 <svwsv3462@gmail.com>
2023-12-28 20:00:58 +0900

    [Error] 추후 수정
    1. 맞았을 때 애니메이션 적용 x
    2. 근접 몬스터 공격에 캐릭터 HP 감소x

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,6 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,16 +12,55 @@ public class GameManager : MonoBehaviour
 
     public Transform Player {  get; private set; }
     [SerializeField] public string playerTag = "Player";
+    private HealthSystem playerHealthSystem;
+
+    [SerializeField] private TextMeshProUGUI waveText;
+    [SerializeField] private Slider hpGaugeSlider;
+
+    [SerializeField] private GameObject gameOverUI;
 
     private void Awake()
     {
         Instance = this;
         Player = GameObject.FindGameObjectWithTag(playerTag).transform;
+
+        playerHealthSystem = Player.GetComponent<HealthSystem>();
+        playerHealthSystem.OnDamage += UpdateHealthUI;
+        playerHealthSystem.OnHeal += UpdateHealthUI;
+        playerHealthSystem.OnDeath += GameOver;
+
+        gameOverUI.SetActive(false);
     }
+
+    private void GameOver()
+    {
+        gameOverUI.SetActive(true);
+    }
+
+    private void UpdateHealthUI()
+    {
+        hpGaugeSlider.value = playerHealthSystem.CurrentHealth / playerHealthSystem.Maxhealth;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameOverUI.SetActive(true);
+    }
+
+    private void UpdateWaveUI()
+    {
+
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 
     // Update is called once per frame
